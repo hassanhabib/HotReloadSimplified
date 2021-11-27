c047a24a567aa568e2f610491ae271c4b47852ee
Hassan Habib <hassanhabib@live.com>
2021-11-27 14:39:36 -0800

    Add project files.

---
+++ HotReloadDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+
+namespace HotReloadDemo
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("Please press Enter:");
+                Console.ReadKey();
+
+                string code =
+                    File.ReadAllText(
+                        @"C:\Users\hassan\source\repos\HotReloadSimplified\HotReloadDemo\DynamicCode.txt");
+
+                await CSharpScript.RunAsync(code);
+            }
+        }
+    }
+}
